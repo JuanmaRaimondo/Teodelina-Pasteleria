ae38b53dcdfc5b2ae926758b7b4c5ced26ab212a
Juan Martin Raimondo <juanmarraimondo@gmail.com>
2026-02-20 19:30:33 -0300

    Commit inicial con la estructura base

---
+++ teodelina/teodelina/src/main/java/com/pasteleria/teodelina/TeodelinaApplication.java
@@ -0,0 +1,13 @@
+package com.pasteleria.teodelina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeodelinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeodelinaApplication.class, args);
+	}
+
+}
---
+++ teodelina/teodelina/src/test/java/com/pasteleria/teodelina/TeodelinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pasteleria.teodelina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeodelinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
